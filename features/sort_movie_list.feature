afc0235a871652c51d83e53937bbb16d86c147a2
Ian Ingram <ian-ingram@uiowa.edu>
2016-10-07 19:44:58 +0000

    Commit for submission

--- features/sort_movie_list.feature
+++ features/sort_movie_list.feature
@@ -23,7 +23,26 @@ Background: movies have been added to database
 
 Scenario: sort movies alphabetically
   # your scenario here
+  When I click Movie_Title Heading to sort by "Movie Title"
+  Then I should see "2001: A Space Odyssey" before "Aladdin"
+  Then I should see "Aladdin" before "Amelie"
+  Then I should see "Amelie" before "Chicken Run"
+  Then I should see "Chicken Run" before "Chocolat"
+  Then I should see "Chocolat" before "Raiders of the Lost Ark"
+  Then I should see "Raiders of the Lost Ark" before "The Help"
+  Then I should see "The Help" before "The Incredibles"
+  Then I should see "The Incredibles" before "The Terminator"
+  Then I should see "The Terminator" before "When Harry Met Sally"
 
 Scenario: sort movies in increasing order of release date
   # your scenario here
-
+  When I click Release_Date Heading to sort by "Release Date"
+  Then I should see "1968-04-06" before "1981-06-12"
+  Then I should see "1981-06-12" before "1984-10-26"
+  Then I should see "1984-10-26" before "1989-07-21"
+  Then I should see "1989-07-21" before "1992-11-25"
+  Then I should see "1992-11-25" before "2000-06-21"
+  Then I should see "2000-06-21" before "2001-01-05"
+  Then I should see "2001-01-05" before "2001-04-25"
+  Then I should see "2001-04-25" before "2004-11-05"
+  Then I should see "2004-11-05" before "2011-08-10"
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -46,12 +46,9 @@ Given /^I am on the RottenPotatoes home page$/ do
 # Add a declarative step here for populating the DB with movies.
 
 Given /the following movies have been added to RottenPotatoes:/ do |movies_table|
-  pending  # Remove this statement when you finish implementing the test step
+  #pending  # Remove this statement when you finish implementing the test step
   movies_table.hashes.each do |movie|
-    # Each returned movie will be a hash representing one row of the movies_table
-    # The keys will be the table headers and the values will be the row contents.
-    # Entries can be directly to the database with ActiveRecord methods
-    # Add the necessary Active Record call(s) to populate the database.
+    Movie.create(movie)    # Add the necessary Active Record call(s) to populate the database.
   end
 end
 
@@ -59,16 +56,108 @@ When /^I have opted to see movies rated: "(.*?)"$/ do |arg1|
   # HINT: use String#split to split up the rating_list, then
   # iterate over the ratings and check/uncheck the ratings
   # using the appropriate Capybara command(s)
-  pending  #remove this statement after implementing the test step
+  #pending  #remove this statement after implementing the test step
+  #puts arg1
+  #puts arg1.split(', ') 
+  
+    Movie.all_ratings.each do |x|
+        #puts "#{x}"
+        uncheck("ratings_#{x}")
+    end
+
+    arg1.split(', ').each do |x|
+        puts "Checked: #{x}"
+        check("ratings_#{x}")
+    end
+
+    click_on "ratings_submit"
 end
 
 Then /^I should see only movies rated: "(.*?)"$/ do |arg1|
-  pending  #remove this statement after implementing the test step
+  #pending  #remove this statement after implementing the test step
+    allratings = Movie.all_ratings
+  
+    displayratings=arg1.split(', ')
+    puts displayratings
+    
+    displayratings.each do |rating|
+        allratings.delete(rating)
+    end
+    puts allratings
+    
+     result = false
+     all("tbody tr").each do |tr| #tbody removes headed column titles
+         displayratings.each do |rating|
+            puts tr.text 
+            if tr.has_content?(rating)
+              result = true
+            else
+              result = false
+              break
+            end
+         end
+    end 
+  expect(result).to be_truthy
 end
 
 Then /^I should see all of the movies$/ do
-  pending  #remove this statement after implementing the test step
+  #pending  #remove this statement after implementing the test step
+  result = false
+  allMovies = Movie.all
+  allMovies.each do |movie|
+    count = 0
+    all("tbody tr").each do |tr|  #tbody removes headed column titles
+        #puts tr.text
+        if tr.has_content?(movie.rating) && tr.has_content?(movie.title) && tr.has_content?(movie.release_date)
+            puts movie.title
+            #result = true
+            count = 1
+        end
+    end
+    if count != 0
+        result = true
+    else
+        result = false
+        return false
+    end
+  end
+      
+  expect(result).to be_truthy
 end
 
+When(/^I click Movie_Title Heading to sort by "(.*?)"$/) do |arg1|
+  #pending # express the regexp above with the code you wish you had
+  click_on arg1
+end
 
+Then(/^I should see "(.*?)" before "(.*?)"$/) do |arg1, arg2|
+  #pending # express the regexp above with the code you wish you had
+  result=false
+  i = 0
+  arg = Hash.new
+  all("tbody tr").each do |tr|
+      if tr.has_content?(arg1)
+         arg[i] = arg1
+         i = i + 1
+      elsif tr.has_content?(arg2)
+         arg[i] = arg2
+         i = i + 1
+      end
+  end
+  if(arg[0] == arg1 && arg[1] == arg2)
+     result = true
+  end
+  
+  puts arg
+   
+  expect(result).to be_truthy
+end
 
+When(/^I click Release_Date Heading to sort by "(.*?)"$/) do |arg1|
+  #pending # express the regexp above with the code you wish you had
+  click_on arg1
+end
+
+#Then(/^I should see "(.*?)" before "(.*?)"$/) do |arg1, arg2|
+  #pending # express the regexp above with the code you wish you had
+#end
